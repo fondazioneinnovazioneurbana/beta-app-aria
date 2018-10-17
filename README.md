d95d750bf37d536154119c24a16f3fe83fcb1795
kiki <info@digitigrafo.it>
2018-10-17 17:33:45 +0200

    tested splash e connessione

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Applicazione per il laboratorio partecipato di Bologna sulla qualità dell'aria.
 
 Primi obiettivi:
-1-sapere giornalmente l'IQA
-2-sapere le stazioni di fondo il valore peggiore
-3-avere una frase di approfondimento ogni giorno
+A-sapere giornalmente l'IQA
+B-sapere le stazioni di fondo il valore peggiore
+C-avere una frase di approfondimento ogni giorno
 
 
 Sviluppo:
@@ -17,8 +17,29 @@ Plugin installati:
     $cordova plugin add cordova-plugin-network-information
 
 Tutto il progetto web è in www. Dentro a www usare "bower install", che scaricherà le dipendenze.
+"dependencies": {
+    "framework7": "^2.0.7",
+    "normalize.css": "^8.0.0",
+    "jquery": "^3.3.1"
+  }
 
-!! Policy sulle risorse:
-Stappata:    
+Raggiungere il primo risultato A:
+
+1- avere lo splashscreen ed una app che si avvia
+Doc plugin: https://cordova.apache.org/docs/en/latest/reference/cordova-plugin-splashscreen/index.html
+
+2- accedere al network.
+Doc plugin: https://cordova.apache.org/docs/en/latest/reference/cordova-plugin-network-information/index.html
+
+3- avere il toaster che ti avverte in caso il tuo network non funzionasse.
+
+3- adattare la Policy sulle risorse:
+Stappata:
  <meta http-equiv="Content-Security-Policy" content="default-src * 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *; img-src 'self' data: content:;">
+
+4- scrivere la richiesta CORS
+
+5- esporre il dato
+
+Raggiungere il risultato B:
 
--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,7 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -26,12 +26,14 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
+        //display_results("h1", "evvai");
+        checkConnection();
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -44,3 +46,33 @@ var app = {
 };
 
 app.initialize();
+
+/*///////////////inizio utilities//////////////*/
+
+function display_results(contenitore, messaggio) {
+    $(contenitore).text(messaggio);
+};
+
+/*per network info*/
+function checkConnection() {
+    var networkState = navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+    if (states[networkState] == 'Unknown connection' || states[networkState] == 'No network connection') {
+        display_results("h1", "noo");
+        //alert('Non sei connesso ad internet, connettiti ad una rete per procedere.');
+        return false
+
+    } else {
+        display_results("h1", "ok");
+        return true
+    }
+}
+/*///////////////fine utilities//////////////*/
