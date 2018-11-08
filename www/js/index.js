9a1b5b5379a1b66abbc2d82f46791484b3933b71
kiki <info@digitigrafo.it>
2018-11-08 20:43:15 +0100

    added social plugin

--- www/js/index.js
+++ www/js/index.js
@@ -416,3 +416,25 @@ function calcolagradiente() {
     $("div.block.rainbow ").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
     //$("div.block.rainbow ").css("animation", "none");
 };
+
+///////////////share plugin ///////////////////
+
+// this is the complete list of currently supported params you can pass to the plugin (all optional)
+var options = {
+    message: 'share this', // not supported on some apps (Facebook, Instagram)
+    subject: 'the subject', // fi. for email
+    files: ['', ''], // an array of filenames either locally or remotely
+    url: 'https://www.website.com/foo/#bar?a=b',
+    chooserTitle: 'Pick an app', // Android only, you can override the default share sheet title,
+    appPackageName: 'com.apple.social.facebook' // Android only, you can provide id of the App you want to share with
+};
+
+var onSuccess = function (result) {
+    console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+    console.log("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+};
+
+var onError = function (msg) {
+    console.log("Sharing failed with message: " + msg);
+};
+
